f45ab5103e67f8086ac18c632a92dcf34a42db97
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-17 11:54:44 -0800

    added the final advanced task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int x;
+int i;
+
+for (i = 48; i < 58; i++)
+{
+for (x = i; x < 58; x++)
+{
+if (i == x)
+{
+continue;
+}
+
+putchar(i);
+putchar(i);
+putchar(32);
+putchar(i);
+putchar(x);
+if (x < 57 || i < 57)
+{
+if (i >= 56)
+{
+continue;
+}
+putchar(44);
+putchar(32);
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
