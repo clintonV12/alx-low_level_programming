8ef1724ae0d8719adb20a9740cbd85729d7f2ed3
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-21 14:23:01 -0800

    re-wrote 103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-long int a;
-long int b;
-long int c;
-long int sum;
+int a;
+int b;
+int c;
+int sum;
 
 a = 1;
 b = 2;
-sum = 0;
-
+sum = sum + b;
 while (c <= 4000000)
 {
 c = a + b;
@@ -26,6 +25,7 @@ if (c % 2 == 0)
 sum = sum + c;
 }
 }
-printf("%ld\n", sum);
+
+printf("%d\n", sum);
 return (0);
 }
