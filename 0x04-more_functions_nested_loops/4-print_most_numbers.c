0cf2e30745b83c5090bb5a26556503fdc68b6f32
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-23 05:20:00 -0800

    added function to print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * print_most_numbers - prints numbers 0 - 9 except 2 & 4
+ *
+ * Return: Always void
+ */
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+if (i == 50 || i == 52)
+{
+continue;
+}
+_putchar(i);
+}
+_putchar('\n');
+}
