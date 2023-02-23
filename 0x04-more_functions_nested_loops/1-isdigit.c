58b3a909f21926b83e1ea6535cabb85a22d27202
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-23 04:50:06 -0800

    created digit checker

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * _isdigit - checks if character is upper case
+ *
+ * @c: the character to check
+ * 
+ * Return: Always int (1 if is upper and 0 otherwise)
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
