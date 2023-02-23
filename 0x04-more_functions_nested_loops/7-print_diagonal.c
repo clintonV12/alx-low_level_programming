c26581d6b5acb4d6bbd7ec0a103cf1a38fe0f4f9
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-23 07:42:52 -0800

    added the doagonal printer

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/*
+ * print_diagonal - prints a line using _
+ * @n: number of times to print n
+ * Return: Always void
+ */
+void print_diagonal(int n)
+{
+int i;
+int h;
+int j;
+j = 32;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+
+for (h = 1; h <= n; h++)
+{
+for (i = 1; i <= h; i++)
+{
+if (i == h)
+{
+j = 92;
+_putchar(j);
+_putchar('\n');
+}
+else if (i < h)
+{
+j = 32;
+_putchar(j);
+}
+}
+
+}
+}
