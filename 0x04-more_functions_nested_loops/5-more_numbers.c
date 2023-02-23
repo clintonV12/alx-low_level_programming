a2e6b82fa2eed2e535584b2fa60e1b6e313c3ae1
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-23 12:41:58 -0800

    fixed bug in 5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
  */
 void more_numbers(void)
 {
-int i;
 int j;
 for (j = 0; j <= 10; j++)
 {
 int k;
+int i;
 k = 48;
 for (i = 48; i <= 63; i++)
 {
-if (i > 57 && i <= 63)
+if (i >= 58 && i <= 63)
 {
 int d;
 d = 49;
@@ -24,10 +24,14 @@ k = 0;
 d = 10;
 }
 _putchar(d);
+if (i == 63)
+{
+break;
+}
 _putchar(k);
 k++;
 }
-else
+else if (i >= 48 && i <= 57)
 {
 _putchar(i);
 }
