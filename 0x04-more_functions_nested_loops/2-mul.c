020031b3379b2548bd53225764f52f3bc22b2bb1
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-23 04:57:15 -0800

    added the number multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * mul - checks if character is upper case
+ *
+ * @a: the int to multiply
+ * @b: the int to multiply
+ * Return: Always int (product of a and b)
+ */
+int mul(int a, int b)
+{
+int p;
+p = a * b;
+return (p);
+}
