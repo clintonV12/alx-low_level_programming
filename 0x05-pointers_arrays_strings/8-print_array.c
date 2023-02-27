781288978bc314c48f370e891de24e48ceb3f55e
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-27 13:03:27 -0800

    added number printer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of array
+ * @a: the array
+ * @n: the number of elements to print
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i < n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d\n", a[i]);
+}
+}
+}
