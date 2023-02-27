57a70fb1a575b34eda6920b33ebdc322a56d5a0f
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-27 13:17:42 -0800

    added string copier

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - cpies string to buffer
+ * @dest: pointer to buffer
+ * @src: pointer to string
+ * Return: char pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+*dest = src;
+return dest;
+}
