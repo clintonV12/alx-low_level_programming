419ce7ac3fa2e5af93c5e87688db4370cd403bef
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-27 12:49:28 -0800

    added half printer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * @str: the string to print
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int len;
+len = 0;
+
+while (str[len] != '\0')
+{
+len++;
+}
+
+if (len % 2 == 0)
+{
+int h;
+int i;
+h = len / 2;
+
+for (i = h; i < len; i++)
+{
+_putchar(str[i]);
+}
+}
+else if (len % 2 != 0)
+{
+int h;
+int i;
+
+h = (len - 1) / 2;
+for (i = h; i < len; i++)
+{
+_putchar(str[i]);
+}
+}
+
+_putchar('\n');
+}
