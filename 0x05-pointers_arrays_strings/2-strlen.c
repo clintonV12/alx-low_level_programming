31bfe4f1225e3b1745d8833ff040bdfece220e15
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-27 11:34:50 -0800

    added string counter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the pointer to the string
+ * Return: always int (the lenth of the string
+ */
+
+int _strlen(char *s)
+{
+int len;
+int i;
+i = 0;
+len = 0;
+
+while (*s[i] != '\0')
+{
+i++
+}
+len = i;
+return len;
+}
