ede18104d72b860236b0f21d1b50fcab16baf286
Clinton Vilakati <vilakaticlinton@gmail.com>
2023-02-27 12:05:14 -0800

    added string reveser

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: the string to reverse
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int len;
+int i;
+int j;
+char *temp;
+
+len = 0;
+
+while (s[len] != '\0')
+{
+len++;
+}
+
+j = 0;
+for (i = len - 1; i >= 0; i--)
+{
+temp[j] = s[i];
+j++;
+}
+
+s = temp;
+}
