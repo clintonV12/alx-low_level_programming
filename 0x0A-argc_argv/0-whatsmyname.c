db5a29bc2fc6f271eb1adee076cd845f0334f97d
clinton <clinton@localhost.localdomain>
2023-06-09 22:37:00 +0200

    initial commit on variable arguments excercise

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf(argv[0]);
+}
+printf("\n");
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+excercise about arguments passed to your program
