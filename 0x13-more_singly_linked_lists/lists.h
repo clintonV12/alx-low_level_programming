64561c1c77903b88922dacceeeaed2ad1b6ab4b8
clinton <clinton@localhost.localdomain>
2023-07-04 18:45:45 +0200

    added function to pop head node of a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
+#include <stdlib.h>
+
 
 /**
  * struct listint_s - singly linked list
@@ -23,5 +25,6 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 
 #endif
